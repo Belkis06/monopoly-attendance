8f331b67ecbe2aa01f328adaf9952ce1fd26620d
cecilia <est.cortezc0201@uml.edu.ni>
2025-11-10 18:10:00 -0600

    coneccion de base de datos en clever cloud

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 import mysql from "mysql2/promise";
 
 export const db = await mysql.createConnection({
-  host: "127.0.0.1",
+  host: "bnw4nsactvtxlhijrkr1-mysql.services.clever-cloud.com",
   port: 3306,
-  user: "root",
-  password: "1234",
-  database: "monopoly_attendance"
+  user: "u7xcbg0zym9pquju",
+  password: "S4HNdBjggT2HL77zxQkz",
+  database: "bnw4nsactvtxlhijrkr1"
 });
 
-console.log("✅ Conectado a MariaDB");
+console.log("✅ Conectado a Clever Cloud MySQL");
